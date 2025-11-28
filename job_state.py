54082d6905ec802d456bc8b3d2a723074b089008
H3199 <eerohaavisto@gmail.com>
2025-11-28 19:54:33 +0200

    Cleaning up the repo...

--- job_state.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-class JobState(str, Enum):
-    NEW = "new"
-    SAVED = "saved"
-    APPLIED = "applied"
-    INTERVIEW = "interview"
-    REJECTED = "rejected"
-    OFFER = "offer"
